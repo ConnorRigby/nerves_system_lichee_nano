698d0fd7551aba14e239ed5fcfc2becdf09c54f6
connor rigby <konnorrigby@gmail.com>
2020-01-14 19:15:10 -0800

    Update mixfile for publish

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule NervesSystemLicheeNano.MixProject do
       description: description(),
       package: package(),
       deps: deps(),
-      aliases: [loadconfig: [&bootstrap/1], docs: ["docs", &copy_images/1]],
+      aliases: [loadconfig: [&bootstrap/1]],
       docs: [extras: ["README.md"], main: "readme"]
     ]
   end
@@ -35,7 +35,7 @@ defmodule NervesSystemLicheeNano.MixProject do
     [
       type: :system,
       artifact_sites: [
-        {:github_releases, "nerves-project/#{@app}"}
+        {:github_releases, "connorrigby/#{@app}"}
       ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
@@ -50,7 +50,7 @@ defmodule NervesSystemLicheeNano.MixProject do
     [
       {:nerves, "~> 1.5.0", runtime: false},
       {:nerves_system_br, "1.10.0", runtime: false},
-      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "1.2.0", runtime: false},
+      {:nerves_toolchain_armv5tejl_unknown_linux_musleabi, "1.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
@@ -58,7 +58,7 @@ defmodule NervesSystemLicheeNano.MixProject do
 
   defp description do
     """
-    Nerves System - Raspberry Pi Zero and Zero W
+    Nerves System - Lichee Nano
     """
   end
 
@@ -66,29 +66,31 @@ defmodule NervesSystemLicheeNano.MixProject do
     [
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/nerves-project/#{@app}"}
+      links: %{"GitHub" => "https://github.com/connorrigby/#{@app}"}
     ]
   end
 
   defp package_files do
     [
       "fwup_include",
+      "package",
       "rootfs_overlay",
+      "boot.scr",
+      "busybox_defconfig",
+      "Config.in",
+      "external.mk",
       "fwup-revert.conf",
       "fwup.conf",
       "LICENSE",
+      "linux_defconfig",
       "mix.exs",
       "nerves_defconfig",
       "post-build.sh",
       "post-createfs.sh",
       "README.md",
+      "uboot_defconfig",
       "VERSION"
     ]
-  end
-
-  # Copy the images referenced by docs, since ex_doc doesn't do this.
-  defp copy_images(_) do
-    File.cp_r("assets", "doc/assets")
   end
 
   defp build_runner_opts() do
@@ -103,7 +105,7 @@ defmodule NervesSystemLicheeNano.MixProject do
     if function_exported?(Mix, :target, 1) do
       apply(Mix, :target, [:target])
     else
-      System.put_env("MIX_TARGET", "target")
+      System.put_env("MIX_TARGET", "lichee_nano")
     end
   end
 end
