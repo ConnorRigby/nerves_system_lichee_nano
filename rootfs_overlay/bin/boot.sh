facd05d0b58f255e66634ca3766388f3e32230e5
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-25 15:08:03 -0500

    Move swap to partition; switch to f2fs
    Moving swap from a file to a dedicated partition simplifies board
    initialization. It also gets rid of the code the reformats the
    application partition on reboot.
    
    This also switches the application partition from ext4 to F2FS to reduce
    the initial format time. F2FS has worked well on other platforms.

--- rootfs_overlay/bin/boot.sh
+++ rootfs_overlay/bin/boot.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-APPDATA_PART=/dev/mmcblk0p3
-APPDATA_MOUNT=/root
-SWAPFILE=$APPDATA_MOUNT/swapfile
+SWAPFILE=/dev/mmcblk0p3
 
-# todo only format partition once
-mkfs.ext4 -F $APPDATA_PART
-mount -t ext4 -o rw $APPDATA_PART $APPDATA_MOUNT
-dd if=/dev/zero of=$SWAPFILE bs=1MB count=100
-chmod 600 $SWAPFILE
 mkswap $SWAPFILE
 swapon $SWAPFILE
+
